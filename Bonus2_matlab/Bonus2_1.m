75e639de278ba0b85102f6fb4c504c347f3e9309
JeffWu86 <b04901043@ntu.edu.tw>
2019-05-28 11:25:19 +0800

    20190528_v1

--- Bonus2_matlab/Bonus2_1.m
+++ Bonus2_matlab/Bonus2_1.m
@@ -1,54 +1,66 @@
 
-[S0,K,r,q,sigma,T,Smin,Smax,m,n]=readdata('input_1.txt');
+[S0,K,r,q,sigma,T,Smin,Smax,m,n]=readdata('input_3.txt');
 tic;
 %Implicit methond
 [Im_AC, Im_AP, Im_EC, Im_EP] = Implicit(S0,K,r,q,sigma,T,Smin,Smax,m,n);
+optType='EURO_CALL';
+dS=(Smax-Smin)/m;
+dT=T/n;
+%Boundary condition for price
+price(1:m+1,1:n+1)=nan;
+switch optType
+    case 'EURO_CALL'
+        price(1,:)=m*dS-K;
+        price(end,:)=max(Smin-K,0);
+        %Stock price in time T
+        for i=1:m+1
+            price(i,n+1)=max((m-i+1)*dS-K,0);
+        end
+    case 'EURO_PUT'
+        price(1,:)=max(K-m*dS,0);
+        price(end,:)=K-Smin;
+        %Stock price in time T
+        for i=1:m+1
+            price(i,n+1)=max(K-(m-i+1)*dS,0);
+        end
+end
 
-deltaS=(Smax-Smin)/m;
-deltaT=T/n;
-price=zeros(m+1,n+1);
-for i=1:n+1
-    price(1,i)=m*deltaS-K;
-    price(n+1,i)=max(Smin-K,0);
-end
-for i=1:m+1
-    price(i,n+1)=max((m-i+1)*deltaS-K,0);
-end
-avec=zeros(1,m-1);
-bvec=zeros(1,m-1);
-cvec=zeros(1,m-1);
-for i=1:m-1
-    avec(1,i)=(r-q)/2*i*deltaT-0.5*power(sigma*i,2)*deltaT;
-    bvec(1,i)=1+power(sigma*i,2)*deltaT+r*deltaT;
-    cvec(1,i)=-(r-q)/2*i*deltaT-0.5*power(sigma*i,2)*deltaT;
-end
+% coef of aj, bj, cj
+i=1:m-1;
+aj=(r-q)/2*i*dT-0.5*power(sigma*i,2)*dT;
+bj=1+power(sigma*i,2)*dT+r*dT;
+cj=-(r-q)/2*i*dT-0.5*power(sigma*i,2)*dT;
+
+% A matrix
 impmat=zeros(m-1,m-1);
 for i=1:m-1
-    impmat(i,i)=bvec(1,i);
+    impmat(i,i)=bj(1,m-i);
     if(i~=1)
-        impmat(i,i-1)=cvec(1,i);
+        impmat(i,i-1)=cj(1,m-i);
     end
     if(i~=m-1)
-        impmat(i,i+1)=avec(1,i);
+        impmat(i,i+1)=aj(1,m-i);
     end
 end
-invimpmat=inv(impmat);
-for i=n:-1:1
-    b=price(2:m,i+1);
-    b(m-1,1)=b(m-1,1)-avec(1,1)*price(m+1,i);
-    b(1,1)=b(1,1)-cvec(1,m-1)*price(1,i);
+
+[L,U] = lu(impmat);
+for idx=n:-1:1
+    b=price(2:m,idx+1);
+    b(end)=b(end)-aj(1)*price(m+1,idx); %Alwayw zero
+    b(1)=b(1)-cj(end)*price(1,idx);
     %x=impmat\b;
-    x=invimpmat*b;
+    x=U\(L\b);
     for j=1:m-1
-        if(b(j,1)<0) 
-            b(j,1)=0;
+        if(x(j,1)<0) 
+            x(j,1)=0;
         end
     end
-    price(2:m,i)=x;
+    price(2:m,idx)=x;
 end
 pre_t=toc;
 fprintf('Compute %f (s)\n',pre_t);
-euro_call=price(m+1-S0/deltaS,1);
+
+euro_call=interp1(Smax:-dS:Smin,price(:,1),S0);
 fprintf('Euro call is %f dollars\n',euro_call);
 % fprintf('Basic requirement: \nStandard Deviation: %f\n',std(basic1));
 % fprintf('Under %d times of testing, 95%% of confidence interval for the call option value are: \n',repeat_times);
---
+++ Bonus2_matlab/test.m
@@ -0,0 +1,87 @@
+X=60;
+S0=50;
+r=0.05;
+sig=0.2;
+Svec=0:0.05:100;
+tvec=0:0.0005:1;
+oType='CALL';
+tic;
+% Function to calculate the price of a vanilla European
+% Put or Call option using the implicit finite difference method
+%
+% oPrice = finDiffImplicit(X,r,sig,Svec,tvec,oType)
+%
+% Inputs: X - strike
+%       : S0 - stock price
+%       : r - risk free interest rate
+%       : sig - volatility
+%       : Svec - Vector of stock prices (i.e. grid points)
+%       : tvec - Vector of times (i.e. grid points)
+%       : oType - must be 'PUT' or 'CALL'.
+%
+% Output: oPrice - the option price
+%
+% Notes: This code focuses on details of the implementation of the
+%        implicit finite difference scheme.
+%        It does not contain any programatic essentials such as error
+%        checking.
+%        It does not allow for optional/default input arguments.
+%        It is not optimized for memory efficiency, speed or
+%        use of sparse matrces.
+
+% Author: Phil Goddard (phil@goddardconsulting.ca)
+% Date  : Q4, 2007
+
+% Get the number of grid points
+M = length(Svec)-1;
+N = length(tvec)-1;
+% Get the grid sizes (assuming equi-spaced points)
+dt = tvec(2)-tvec(1);
+
+% Calculate the coefficients
+% To do this we need a vector of j points
+j = 0:M;
+sig2 = sig*sig;
+aj = (dt*j/2).*(r - sig2*j);
+bj = 1 + dt*(sig2*(j.^2) + r);
+cj = -(dt*j/2).*(r + sig2*j);
+
+% Pre-allocate the output
+price(1:M+1,1:N+1) = nan;
+
+% Specify the boundary conditions
+switch oType
+    case 'CALL'
+        % Specify the expiry time boundary condition
+        price(:,end) = max(Svec-X,0);
+        % Put in the minimum and maximum price boundary conditions
+        % assuming that the largest value in the Svec is
+        % chosen so that the following is true for all time
+        price(1,:) = 0;
+        price(end,:) = (Svec(end)-X)*exp(-r*tvec(end:-1:1));
+    case 'PUT'
+        % Specify the expiry time boundary condition
+        price(:,end) = max(X-Svec,0);
+        % Put in the minimum and maximum price boundary conditions
+        % assuming that the largest value in the Svec is
+        % chosen so that the following is true for all time
+        price(1,:) = (X-Svec(end))*exp(-r*tvec(end:-1:1));
+        price(end,:) = 0;
+end
+
+% Form the tridiagonal matrix
+B = diag(aj(3:M),-1) + diag(bj(2:M)) + diag(cj(2:M-1),1);
+[L,U] = lu(B);
+
+% Solve at each node
+offset = zeros(size(B,2),1);
+for idx = N:-1:1
+    offset(1) = aj(2)*price(1,idx);
+    offset(end) = cj(end)*price(end,idx); % This will always be zero
+    price(2:M,idx) = U\(L\(price(2:M,idx+1) - offset));
+end
+
+% Calculate the option price
+oPrice = interp1(Svec,price(:,1),S0)
+pre_t=toc;
+fprintf('Compute %f (s)\n',pre_t);
